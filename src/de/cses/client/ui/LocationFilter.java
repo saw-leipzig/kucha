621a9d5d1d2737eb16ca057845e8edebd1c026c9
Andreas Lingnau <andreas.lingnau@hu-berlin.de>
2018-02-02 15:24:42 +0100

    improved district selection in filter

--- src/de/cses/client/ui/LocationFilter.java
+++ src/de/cses/client/ui/LocationFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 
+ * Copyright 2017 - 2018
  * Saxon Academy of Science in Leipzig, Germany
  * 
  * This is free software: you can redistribute it and/or modify it under the terms of the 
@@ -19,8 +19,11 @@ import java.util.List;
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.text.shared.AbstractSafeHtmlRenderer;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.cell.core.client.SimpleSafeHtmlCell;
+import com.sencha.gxt.core.client.IdentityValueProvider;
+import com.sencha.gxt.core.client.Style.SelectionMode;
 import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.core.client.XTemplates;
 import com.sencha.gxt.core.client.util.Margins;
@@ -29,12 +32,14 @@ import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
 import com.sencha.gxt.widget.core.client.ContentPanel;
+import com.sencha.gxt.widget.core.client.ListView;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
 import com.sencha.gxt.widget.core.client.form.DualListField;
+import com.sencha.gxt.widget.core.client.form.DualListField.DualListFieldAppearance;
+import com.sencha.gxt.widget.core.client.grid.CheckBoxSelectionModel;
+import com.sencha.gxt.widget.core.client.selection.StoreSelectionModel;
 
-import de.cses.client.DatabaseService;
-import de.cses.client.DatabaseServiceAsync;
 import de.cses.client.StaticTables;
 import de.cses.shared.DistrictEntry;
 import de.cses.shared.RegionEntry;
@@ -46,7 +51,6 @@ import de.cses.shared.SiteEntry;
  */
 public class LocationFilter extends AbstractFilter {
 
-	private final DatabaseServiceAsync dbService = GWT.create(DatabaseService.class);
 	private SiteProperties siteProps;
 	private ListStore<SiteEntry> siteEntryList;
 	private RegionProperties regionProps;
@@ -54,11 +58,11 @@ public class LocationFilter extends AbstractFilter {
 	private DistrictProperties districtProps;
 	private ListStore<DistrictEntry> districtEntryList;
 	private DualListField<SiteEntry, String> siteSelection;
-	private DualListField<DistrictEntry, String> districtSelection;
 	private DualListField<RegionEntry, String> regionSelection;
 	private ListStore<SiteEntry> selectedSitesList;
 	private ListStore<DistrictEntry> selectedDistrictsList;
 	private ListStore<RegionEntry> selectedRegionsList;
+	private ListView<DistrictEntry, DistrictEntry> districtSelectionLV;
 
 	interface DistrictProperties extends PropertyAccess<DistrictEntry> {
 		ModelKeyProvider<DistrictEntry> districtID();
@@ -67,8 +71,8 @@ public class LocationFilter extends AbstractFilter {
 	}
 	
 	interface DistrictViewTemplates extends XTemplates {
-		@XTemplate("<div>{name}</div>")
-		SafeHtml districtLabel(String name);
+		@XTemplate("<div>{districtName}<br>{siteName}</div><hr>")
+		SafeHtml districtLabel(String districtName, String siteName);
 	}
 	
 	interface RegionProperties extends PropertyAccess<RegionEntry> {
@@ -78,18 +82,20 @@ public class LocationFilter extends AbstractFilter {
 	}
 	
 	interface RegionViewTemplates extends XTemplates {
-		@XTemplate("<div>{phoneticName}<br>{originalName}<br>{englishName}</div>")
-		SafeHtml regionLabel(String phoneticName, String originalName, String englishName);
+		@XTemplate("<div>{englishName}<br>{phoneticName} - {originalName}<br>{siteName}</div><hr>")
+		SafeHtml regionLabel(String englishName, String phoneticName, String originalName, String siteName);
 		
-		@XTemplate("<div>{englishName}</div>")
-		SafeHtml regionLabel(String englishName);
+		@XTemplate("<div>{englishName}<br>{phoneticName}<br>{siteName}</div><hr>")
+		SafeHtml regionLabel(String englishName, String phoneticName, String siteName);
+		
+		@XTemplate("<div>{englishName}<br>{siteName}</div><hr>")
+		SafeHtml regionLabel(String englishName, String siteName);
 	}
 	
 	interface SiteProperties extends PropertyAccess<SiteEntry> {
 		ModelKeyProvider<SiteEntry> siteID();
 		LabelProvider<SiteEntry> uniqueID();
 		ValueProvider<SiteEntry, String> name();
-		
 	}
 	
 	interface SiteViewTemplates extends XTemplates {
@@ -109,7 +115,7 @@ public class LocationFilter extends AbstractFilter {
 		districtProps = GWT.create(DistrictProperties.class);
 		districtEntryList = new ListStore<DistrictEntry>(districtProps.districtID());
 		selectedDistrictsList  = new ListStore<DistrictEntry>(districtProps.districtID());
-
+		
 		loadSites();		
 		loadDistricts();
 		loadRegions();
@@ -127,20 +133,32 @@ public class LocationFilter extends AbstractFilter {
 		siteSelection.getDownButton().removeFromParent();
 		siteSelection.getUpButton().removeFromParent();
 		siteSelection.setMode(DualListField.Mode.INSERT);
+		
 		ContentPanel sitePanel = new ContentPanel();
 		sitePanel.setHeaderVisible(true);
 		sitePanel.setHeading("Sites");
 		sitePanel.add(siteSelection);
 		
-		districtSelection = new DualListField<DistrictEntry, String>(districtEntryList, selectedDistrictsList, districtProps.name(), new TextCell());
-		districtSelection.setEnableDnd(true);
-		districtSelection.getDownButton().removeFromParent();
-		districtSelection.getUpButton().removeFromParent();
-		districtSelection.setMode(DualListField.Mode.INSERT);
+		districtSelectionLV = new ListView<DistrictEntry, DistrictEntry>(districtEntryList, new IdentityValueProvider<DistrictEntry>(){}, new SimpleSafeHtmlCell<DistrictEntry>(new AbstractSafeHtmlRenderer<DistrictEntry>() {
+			final DistrictViewTemplates dvTemplates = GWT.create(DistrictViewTemplates.class);
+
+			@Override
+			public SafeHtml render(DistrictEntry entry) {
+				SiteEntry se = siteEntryList.findModelWithKey(Integer.toString(entry.getSiteID()));
+				return dvTemplates.districtLabel(entry.getName(), se.getName());
+			}}));
+		districtSelectionLV.getSelectionModel().setSelectionMode(SelectionMode.SIMPLE);
+		
+//		DualListField<DistrictEntry, String> districtSelection = new DualListField<DistrictEntry, String>(districtEntryList, selectedDistrictsList, districtProps.name(), new TextCell());
+//		districtSelection.setEnableDnd(true);
+//		districtSelection.getDownButton().removeFromParent();
+//		districtSelection.getUpButton().removeFromParent();
+//		districtSelection.setMode(DualListField.Mode.INSERT);
+		
 		ContentPanel districtPanel = new ContentPanel();
 		districtPanel.setHeaderVisible(true);
 		districtPanel.setHeading("Districts");
-		districtPanel.add(districtSelection);
+		districtPanel.add(districtSelectionLV);
 		
 		regionSelection = new DualListField<RegionEntry, String>(regionEntryList, selectedRegionsList, regionProps.englishName(), new TextCell());
 		regionSelection.setEnableDnd(true);
@@ -202,15 +220,23 @@ public class LocationFilter extends AbstractFilter {
 		List<DistrictEntry> nonSelectedDistricts = districtEntryList.getAll();
 		List<RegionEntry> nonSelectedRegions = regionEntryList.getAll();
 		
-		if (selectedDistrictsList.size() > 0) {
-			for (DistrictEntry de : selectedDistrictsList.getAll()) {
-				if (districtQuery.isEmpty()) {
-					districtQuery = "" + de.getDistrictID();
-				} else {
-					districtQuery = districtQuery.concat(", " + de.getDistrictID());
-				}
+		for (DistrictEntry de : districtSelectionLV.getSelectionModel().getSelectedItems()) {
+			if (districtQuery.isEmpty()) {
+				districtQuery = "" + de.getDistrictID();
+			} else {
+				districtQuery = districtQuery.concat(", " + de.getDistrictID());
 			}
 		}
+		
+//		if (selectedDistrictsList.size() > 0) {
+//			for (DistrictEntry de : selectedDistrictsList.getAll()) {
+//				if (districtQuery.isEmpty()) {
+//					districtQuery = "" + de.getDistrictID();
+//				} else {
+//					districtQuery = districtQuery.concat(", " + de.getDistrictID());
+//				}
+//			}
+//		}
 
 		if (selectedRegionsList.size() > 0) {
 			for (RegionEntry re : selectedRegionsList.getAll()) {
