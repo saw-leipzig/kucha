cca6c2ef9adaab5f12630c8ea5599fc027975c58
Andreas Lingnau <lingnau@saw-leipzig.de>
2018-09-25 19:15:25 +0200

    sorting of cave list improved

--- src/de/cses/client/depictions/DepictionFilter.java
+++ src/de/cses/client/depictions/DepictionFilter.java
@@ -14,6 +14,7 @@
 package de.cses.client.depictions;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.safehtml.shared.SafeHtml;
@@ -139,6 +140,47 @@ public class DepictionFilter extends AbstractFilter {
 	private void loadCaves() {
 		dbService.getCaves(new AsyncCallback<ArrayList<CaveEntry>>() {
 
+			private String getComparisonLabel(CaveEntry ce) {
+				StaticTables stab = StaticTables.getInstance();
+				String shortName = stab.getSiteEntries().get(ce.getSiteID()).getShortName();
+				int len = 0;
+				while ((len < ce.getOfficialNumber().length()) && isInteger(ce.getOfficialNumber().substring(0, len+1))) {
+					++len;
+				}
+				switch (len) {
+					case 1:
+						return shortName + "  " + ce.getOfficialNumber();
+					case 2:
+						return shortName + " " + ce.getOfficialNumber();
+					default:
+						return shortName + ce.getOfficialNumber();
+				}
+			}
+			
+			private boolean isInteger(String str) {
+				if (str == null) {
+					return false;
+				}
+				int length = str.length();
+				if (length == 0) {
+					return false;
+				}
+				int i = 0;
+				if (str.charAt(0) == '-') {
+					if (length == 1) {
+						return false;
+					}
+					i = 1;
+				}
+				for (; i < length; i++) {
+					char c = str.charAt(i);
+					if (c < '0' || c > '9') {
+						return false;
+					}
+				}
+				return true;
+			}
+
 			@Override
 			public void onFailure(Throwable caught) {
 				caught.printStackTrace();
@@ -146,6 +188,13 @@ public class DepictionFilter extends AbstractFilter {
 
 			@Override
 			public void onSuccess(ArrayList<CaveEntry> caveResults) {
+				caveResults.sort(new Comparator<CaveEntry>() {
+
+					@Override
+					public int compare(CaveEntry ce1, CaveEntry ce2) {
+						return getComparisonLabel(ce1).compareTo(getComparisonLabel(ce2));
+					}
+				});
 				for (CaveEntry ce : caveResults) {
 					caveEntryLS.add(ce);
 				}
