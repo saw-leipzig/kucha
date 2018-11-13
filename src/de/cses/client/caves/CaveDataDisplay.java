29e55ca8e56d51bb1953e85e1ec77cfc95863388
Andreas Lingnau <lingnau@saw-leipzig.de>
2018-11-13 16:15:08 +0100

    List of CaveSketches added to cave visualisaiton

--- src/de/cses/client/caves/CaveDataDisplay.java
+++ src/de/cses/client/caves/CaveDataDisplay.java
@@ -1,17 +1,42 @@
 package de.cses.client.caves;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.HTML;
 import com.sencha.gxt.widget.core.client.container.MarginData;
 
 import de.cses.client.StaticTables;
 import de.cses.client.ui.AbstractDataDisplay;
+import de.cses.client.user.UserLogin;
 import de.cses.shared.CaveEntry;
+import de.cses.shared.CaveSketchEntry;
+import de.cses.shared.CaveTypeEntry;
 import de.cses.shared.DistrictEntry;
 import de.cses.shared.RegionEntry;
 import de.cses.shared.SiteEntry;
 
 public class CaveDataDisplay extends AbstractDataDisplay {
+	
+	class CaveSketchUri {
+		
+		String uri;
+
+		/**
+		 * @param caveSketch
+		 */
+		public CaveSketchUri(String uri) {
+			this.uri = uri;
+		}
+
+		public String getUri() {
+			return uri;
+		}
+
+		public void setUri(String uri) {
+			this.uri = uri;
+		}
+	}
 	
 	private CaveEntry entry;
 	private StaticTables stab = StaticTables.getInstance();
@@ -31,11 +56,23 @@ public class CaveDataDisplay extends AbstractDataDisplay {
 				re != null ? re.getLabel(): "", 
 				entry.getLastChangedByUser(),
 				entry.getLastChangedOnDate(), 
-				entry.getCaveSketchList()
+				getCaveSketchURIs()
 			));
 		htmlWidget.addStyleName("html-data-display");
 		add(htmlWidget, new MarginData(0, 0, 0, 0));
 		setHeading((se != null ? se.getShortName() : "Site Unknown") + entry.getOfficialNumber() + (!entry.getHistoricName().isEmpty() ? "(" + entry.getHistoricName() + ")" : ""));
+	}
+	
+	private ArrayList<CaveSketchUri> getCaveSketchURIs() {
+		ArrayList<CaveSketchUri> result = new ArrayList<CaveSketchUri>();
+		for (CaveSketchEntry cse : entry.getCaveSketchList()) {
+			result.add(new CaveSketchUri("resource?cavesketch=" + cse.getCaveSketchFilename() + UserLogin.getInstance().getUsernameSessionIDParameterForUri()));
+		}
+		CaveTypeEntry cte = StaticTables.getInstance().getCaveTypeEntries().get(entry.getCaveTypeID());
+		if ((cte != null) && !cte.getSketchName().isEmpty()) {
+			result.add(new CaveSketchUri("resource?background=" + cte.getSketchName() + UserLogin.getInstance().getUsernameSessionIDParameterForUri()));
+		}
+		return result;
 	}
 
 	@Override
--- src/de/cses/client/caves/CaveViewTemplates.java
+++ src/de/cses/client/caves/CaveViewTemplates.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.sencha.gxt.core.client.XTemplates;
 
+import de.cses.client.caves.CaveDataDisplay.CaveSketchUri;
 import de.cses.shared.CaveSketchEntry;
 
 public interface CaveViewTemplates extends XTemplates {
 	
 	@XTemplate(source = "CaveDisplay.html")
-	SafeHtml display(String officialNumber, String historicName, String optionalHistoricName, String site, String district, String region, String user, String timestamp, ArrayList<CaveSketchEntry> caveSketches);
+	SafeHtml display(String officialNumber, String historicName, String optionalHistoricName, String site, String district, String region, String user, String timestamp, ArrayList<CaveSketchUri> caveSketchList);
 
 }
